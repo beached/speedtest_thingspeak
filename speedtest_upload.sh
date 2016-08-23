bba58504c4496b164e94cac28f981822fe82d8cc
Darrell Wright <dwright-git@dawdevel.ca>
2016-08-22 20:20:41 -0400

    parameterized binaries

--- speedtest_upload.sh
+++ speedtest_upload.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export API_KEY=`cat /etc/speedtest_thingspeak.key|tr -d '[[:space:]]'`
+export SPEEDTESTCLI_BIN="/usr/local/bin/speedtest-cli"
+export WGET_BIN="/opt/local/bin/wget"
 
-export results=`speedtest-cli --simple`
+export results=`${SPEEDTESTCLI_BIN} --simple`
+
 #export results="Ping: 75.92 ms
 #Download: 19.40 Mbit/s
 #Upload: 1.02 Mbit/s"
@@ -13,7 +16,7 @@ export ping_test=`echo ${results}|cut -f2 -d' '|tr -d '[[:space:]]'`
 export download_test=`echo ${results}|cut -f5 -d' '|tr -d '[[:space:]]'`
 export upload_test=`echo ${results}|cut -f8 -d' '|tr -d '[[:space:]]'`
 
-export RESULT=`wget -qO- "https://api.thingspeak.com/update?key=${API_KEY}&field1=${ping_test}&field2=${download_test}&field3=${upload_test}"`
+export RESULT=`${WGET_BIN} -qO- "https://api.thingspeak.com/update?key=${API_KEY}&field1=${ping_test}&field2=${download_test}&field3=${upload_test}"`
 
 #echo "PING: ${ping_test}"
 #echo "DOWN: ${download_test}"
